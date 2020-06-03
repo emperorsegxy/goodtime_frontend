bb94706ee5f15c4b5362f56801b7c541c72f8dbb
emperorsegxy <emperorsegxy009gmail.com>
2020-06-03 13:12:19 +0100

    games view done. working on buttons to copy to clipboard and share link

--- src/components/Games/GamesView.jsx
+++ src/components/Games/GamesView.jsx
@@ -3,16 +3,37 @@ import Typography from "@material-ui/core/Typography";
 import {Link} from "react-router-dom";
 import styles from "./games.module.css";
 import {makeStyles} from "@material-ui/styles";
+import Button from "@material-ui/core/Button";
 
 
 const useStyles = makeStyles({
     h4: {
         color: '#FFF',
-        fontSize: 48,
+        fontSize: 58,
+        fontWeight: 'bold',
+        paddingBottom: 50,
     },
     subtitle2: {
         color: '#FFF',
-        fontSize: 28,
+        fontSize: 20,
+        maxWidth: 600,
+        fontWeight: '300',
+    },
+    copyLink: {
+        border: '1px solid #FFD13C',
+        background: 'transparent',
+        color: '#FFF',
+        padding: '10px 38px',
+    },
+    shareLink: {
+        // border: '1px solid #FFD13C',
+        background: '#C6990A',
+        color: '#FFF',
+        padding: '10px 38px',
+        '&:$hover': {
+            background: '#C6990A',
+            opacity: .78,
+        }
     }
 });
 
@@ -22,23 +43,29 @@ const GamesView = (props) => {
 
     return (
         <div
-            style={{
-                backgroundImage: `url(${require('./images/gamesAdv.png')})`,
-                backgroundPosition: 'center',
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat',
-                height: '100%',
-            }} className={styles.gamesViewBackground}
+            // style={{
+            //     backgroundImage: `url(${require('./images/gamesAdv.png')})`,
+            //     backgroundPosition: 'center',
+            //     backgroundSize: 'cover',
+            //     backgroundRepeat: 'no-repeat',
+            //     height: '100%',
+            // }}
+            className={styles.gamesViewBackground}
         >
             <div className="container pt-5 h-100 d-flex flex-column">
-                <div><Link to='/games' href="#">Back home</Link></div>
+                <div><Link to='/games' href="#">Back Home</Link></div>
                 <div className={styles.gamesViewText}>
                     <Typography variant="h4" className={classes.h4}>
                         WAR SHIP RELOADED
                     </Typography>
                     <Typography variant="subtitle2" className={classes.subtitle2}>
-                        Services and issue tracking down no High page performance improvements in Blocks are loaded
+                        Elite Lifestyles transfer the appropriate use of exclusions with regard for better
+                        positive and quality assurance that defines which they become available
                     </Typography>
+                    <div className={styles.actionBtns}>
+                        <Button className={classes.copyLink}>Copy Game Link</Button>
+                        <Button className={classes.shareLink}>Share With Friends</Button>
+                    </div>
                 </div>
             </div>
         </div>
